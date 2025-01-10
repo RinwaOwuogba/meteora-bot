02f0d08743f1c3ce9aac0d5e4c031a34d04f5fbb
Bolarinwa <bhorlarinwah@gmail.com>
2025-01-11 00:57:03 +0100

    increase time between mines

--- src/services/data-engine/data-engine.ts
+++ src/services/data-engine/data-engine.ts
@@ -1,17 +1,11 @@
-import {
-  getDexScreenerPairs,
-  DexScreenerPair,
-} from '../dexscreener/dexscreener';
+import { getDexScreenerPairs } from '../dexscreener/dexscreener';
 import { getJupiterTokenList } from '../jupiter/jupiter';
-import { getMeteoraPairs, MeteoraDlmmPair } from '../meteora/get-meteora-pairs';
+import { getMeteoraPairs } from '../meteora/get-meteora-pairs';
 import { Database } from '@/db/types';
 import { Kysely } from 'kysely';
 import * as fs from 'fs/promises';
 import * as path from 'path';
-import {
-  addMeteoraData,
-  DexScreenerPairEnriched,
-} from '../meteora/get-dlmm-opportunities';
+import { addMeteoraData } from '../meteora/get-dlmm-opportunities';
 import { generateId } from '@/utils/utils';
 
 export class DataEngine {
@@ -87,54 +81,9 @@ export class DataEngine {
       meteoraPairs,
     );
     console.log('Compiled data');
-    console.log(
-      'Size of dexScreener data in memory:',
-      JSON.stringify(dexScreenerPairs).length,
-      'bytes',
-    );
-    console.log(
-      'Size of enriched data in memory:',
-      JSON.stringify(enrichedData).length,
-      'bytes',
-    );
-    console.log(
-      'Size of meteora data in memory:',
-      JSON.stringify(meteoraPairs).length,
-      'bytes',
-    );
 
     return { enrichedData, meteoraPairs, dexScreenerPairs };
   }
-
-  // async fetchAndEnrichData(): Promise<{
-  //   enrichedData: any;
-  //   meteoraPairs: any;
-  //   dexScreenerPairs: any;
-  //   date: string;
-  // }> {
-  //   // Instantiate a shared date field
-  //   const date = new Date().toISOString();
-
-  //   // Mock data
-  //   const meteoraPairs = [
-  //     { address: 'pair-1-address', pairName: 'BTC/ETH', date },
-  //     { address: 'pair-2-address', pairName: 'ETH/USDT', date },
-  //   ];
-
-  //   const dexScreenerPairs = [
-  //     { address: 'pair-1-address', liquidity: 5000, volume: 1000, date },
-  //     { address: 'pair-2-address', liquidity: 8000, volume: 1200, date },
-  //   ];
-
-  //   const enrichedData = meteoraPairs.map((pair) => ({
-  //     ...pair,
-  //     dexScreenerData: dexScreenerPairs.find((d) => d.address === pair.address),
-  //     additionalInfo: `Enriched info for ${pair.pairName}`,
-  //     date,
-  //   }));
-
-  //   return { enrichedData, meteoraPairs, dexScreenerPairs, date };
-  // }
 
   async storeMetricsInDb(
     startTime: Date,
