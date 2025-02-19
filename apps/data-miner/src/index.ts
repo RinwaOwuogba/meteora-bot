fcaae7812558a7fa7ed48792d802781acc35f4d0
Bolarinwa <bhorlarinwah@gmail.com>
2025-02-20 00:40:02 +0100

    remove test scripts

--- apps/data-miner/src/index.ts
+++ apps/data-miner/src/index.ts
@@ -1,10 +1,10 @@
 import {
   config,
-  constants,
+  // constants,
   dataEngine,
   db,
 } from '@meteora-bot-monorepo/shared';
-import fs from 'fs';
+// import fs from 'fs';
 
 let dataEngineInterval: NodeJS.Timeout | null = null;
 
@@ -12,14 +12,14 @@ const main = async (): Promise<void> => {
   const dbConnection = db.createPostgresDBConnection(config.DB_URL);
   await db.migrateToLatest(config.DB_URL);
 
-  const dataIndexer = new dataEngine.DataIndexer(dbConnection);
-  // const dataEngineInstance = new dataEngine.DataEngine(
-  //   dbConnection,
-  //   1000 * 60 * 20,
-  //   config.DATA_DIR,
-  // ); // 20 minutes
-  // dataEngineInterval = dataEngineInstance.executeAtInterval();
-  // console.log('Data engine running');
+  const dataEngineInstance = new dataEngine.DataEngine(
+    dbConnection,
+    1000 * 60 * 20,
+    config.DATA_DIR,
+  ); // 20 minutes
+  dataEngineInterval = dataEngineInstance.executeAtInterval();
+  console.log('Data engine running');
+  // const dataIndexer = new dataEngine.DataIndexer(dbConnection);
   // const file = fs
   //   .readFileSync(
   //     __dirname + '/1739256230990_historical_data_nixcOQnwXq64-NzLFRi-g.json',
@@ -31,16 +31,16 @@ const main = async (): Promise<void> => {
   //   'nixcOQnwXq64-NzLFRi-g',
   // );
   // console.log('indexed!');
-  const pgDbConn = db.createPostgresDBConnection(config.DB_URL);
-  const sqliteDbConn = db.createSqliteDBConnection(
-    '/Users/rinwa/Desktop/dev/web3-madness/meteora-bot/apps/data-miner/db.sqlite',
-  );
-  const crossDBMigrator = new dataEngine.SqliteToPostgresDataMigrator(
-    sqliteDbConn,
-    pgDbConn,
-  );
-  await crossDBMigrator.migrateAllData();
-  console.log('migration complete');
+  // const pgDbConn = db.createPostgresDBConnection(config.DB_URL);
+  // const sqliteDbConn = db.createSqliteDBConnection(
+  //   '/Users/rinwa/Desktop/dev/web3-madness/meteora-bot/apps/data-miner/db.sqlite',
+  // );
+  // const crossDBMigrator = new dataEngine.SqliteToPostgresDataMigrator(
+  //   sqliteDbConn,
+  //   pgDbConn,
+  // );
+  // await crossDBMigrator.migrateAllData();
+  // console.log('migration complete');
 };
 
 main();
