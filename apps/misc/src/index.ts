7d3951ef6c65486cd71e09ebb13a8b097cd67cf0
Bolarinwa <bhorlarinwah@gmail.com>
2025-03-07 19:51:51 +0100

    use update token data scripts in misc

--- apps/misc/src/index.ts
+++ apps/misc/src/index.ts
@@ -4,10 +4,8 @@ const main = async (): Promise<void> => {
   const dbConnection = db.createPostgresDBConnection(config.DB_URL);
   await db.migrateToLatest(config.DB_URL);
 
-  const dataIndexer = new dataEngine.DataIndexer(dbConnection);
-  const defrag = new dataEngine.DefragmentMinedData(dataIndexer);
-  await defrag.defragmentAndIndex(config.DATA_DIR + '/enriched_dex_pairs');
-  console.log('Done with defrag');
+  const tokenDataUpdater = new dataEngine.TokenDataUpdater(dbConnection);
+  await tokenDataUpdater.updateTokenData();
 };
 
 main();
